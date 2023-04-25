2204a26f5ec790a0b3eb51ab2490d585998afd4c
Arczie2022 <116554883+Arczie2022@users.noreply.github.com>
2023-04-25 13:51:18 +0800

    Add files via upload

---
+++ dbtry.php
@@ -0,0 +1,9 @@
+<?php
+	$dbSeverName = "localhost";
+	$dbUsername = "root";
+	$dbUserPassword = "123412341234";
+	$dbName = "detosupdb";
+
+	$conn = mysqli_connect($dbSeverName, $dbUsername, $dbUserPassword, $dbName);
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'Models/Main/CustomToken.php';
+    error_reporting(E_ERROR);
+    
+    echo '<script>location.replace("View/Account/LogIn.php");</script>';
+?>
